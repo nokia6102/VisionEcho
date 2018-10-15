a34d78015ad502cc0d9fc618ffc3a9fabbee40b9
Leon Cheng <nokia6102@gmail.com>
2018-10-15 15:40:45 +0800

    Initial Commit

---
+++ VisionEcho/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionEcho
+//
+//  Created by Leon Cheng on 2018/10/15.
+//  Copyright © 2018年 Leon Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
