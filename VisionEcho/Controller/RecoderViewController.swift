03ecee3f25d71cee6ec2fa945fa25414755c45ad
Leon Cheng <nokia6102@gmail.com>
2018-10-17 20:17:58 +0800

    LaunchScreen改為main 限制畫面只能用直立的 加入錄音程式且可以正常錄音且播放(for iOS12)

--- VisionEcho/Controller/RecoderViewController.swift
+++ VisionEcho/Controller/RecoderViewController.swift
@@ -31,11 +31,8 @@ class RecoderViewController: UIViewController {
         if let number: Int = UserDefaults.standard.object(forKey: "lastestDay") as? Int {
             lastestDay = number
         }
-        
-      
-        
-        //TODO://檢查錄音檔
-        //recodeFileisExist
+
+        //檢查錄音檔
         let thisDay = 1
         let destinationUrl = getDirectoryPath().appendingPathComponent("\(thisDay).m4a")
         let fileExists = FileManager().fileExists(atPath: destinationUrl.path)
@@ -43,70 +40,62 @@ class RecoderViewController: UIViewController {
         if fileExists
         {
             print ("音樂有了")
-//            print("file Exists:\(destinationUrl)")
-//            // prepare file
-//            prepareAudioFile()
         }
-
-        
+        // Configure record session
         configRecordSession()
+        
+      
+    }
+    @IBAction func tapPlay(_ sender:UIButton)
+    {
+        let recordFilePath = getDirectoryPath().appendingPathComponent("1.m4a")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: recordFilePath)
+            audioPlayer.volume = 1.0
+            audioPlayer.play()
+        } catch {
+            print("Play error:", error.localizedDescription)
+        }
     }
     
     @IBAction func tapRecode(_ sender: UIButton)
     {
-        
-        //TODO://檢查錄音檔
-        //recodeFileisExist
-        let thisDay = 1
-        let destinationUrl = getDirectoryPath().appendingPathComponent("\(thisDay).m4a")
-        let fileExists = FileManager().fileExists(atPath: destinationUrl.path)
-        if fileExists
-        {
-            //todo:顯示綠色的正在播放
-            print("file Exists:\(destinationUrl)")
-            print("播放錄音檔")
-            
-        }
-        else
-        {
                 print ("按下錄音鍵")
                 if( enRecode )
                 {
+                    self.recoder()
                     UIView.animate(withDuration: 1.0, animations: {
                         self.btnRecode.alpha = 1.0
                     }) { (Bool) in
-                        self.recoder()
+                        print ("按下錄音鍵的動畫完成")
                     }
                 }
                 else
                 {
                     self.stop()
-                    UIView.animate(withDuration: 1.0) {
+                    UIView.animate(withDuration: 1.0, animations: {
                         self.btnRecode.alpha = 0.3
+                    }) { (Bool) in
+                        self.play()
                     }
+                    
                 }
                 enRecode = !enRecode
-        }
-       
-        
     }
 
 func configRecordSession()
 {
+
         recordingSession = AVAudioSession.sharedInstance()
-        
+    
         do {
-            //old fail
-//            try recordingSession.setCategory(AVAudioSession.Category.playAndRecord)
-//            try recordingSession.setActive(true)
-            
-            //SWift4.2
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
+            //SWift4.2 iOS12 only
+          try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default, options: [])
+          try AVAudioSession.sharedInstance().setActive(true)
+   
             recordingSession.requestRecordPermission() { permissionAllowed in
                 if permissionAllowed {
-                    
+
                 } else {
                     // failed to record!
                 }
@@ -115,7 +104,6 @@ func configRecordSession()
             print("Session error:", error.localizedDescription)
         }
 }
-
     
 func getDirectoryPath() -> URL
 {
@@ -123,31 +111,28 @@ func getDirectoryPath() -> URL
         let fileDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         return fileDirectoryURL
 }
- 
-func prepareAudioFile() {
-
-    let destinationUrl = getDirectoryPath().appendingPathComponent("1.m4a")
-  
-    do {
-        audioPlayer = try AVAudioPlayer(contentsOf: destinationUrl)
-        audioPlayer.prepareToPlay()
-        print ("\(destinationUrl) 檔案準備完成")
-    } catch {
-        print("Error:", error.localizedDescription)
-    }
-}
     
 func play()
     {
-        audioPlayer.play()
+        let destinationUrl = getDirectoryPath().appendingPathComponent("1.m4a")
+        let fileExists = FileManager().fileExists(atPath: destinationUrl.path)
+        if fileExists
+        {
+            print("Check Audio file")
+            print(destinationUrl)
+            
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: destinationUrl)
+                audioPlayer.volume = 1.0
+                audioPlayer.play()
+            } catch {
+                print("Play error:", error.localizedDescription)
+            }
+        }
     }
     
 func recoder()
     {
-//       print("正在錄音...")
-      
-//            let thisDay = 1
-        
             let destinationUrl = getDirectoryPath().appendingPathComponent("1.m4a")
             
             let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
@@ -171,44 +156,6 @@ func stop()
         print("關掉錄音")
         audioRecorder.stop()
         audioRecorder = nil
-        
-        let destinationUrl = getDirectoryPath().appendingPathComponent("1.m4a")
-        let fileExists = FileManager().fileExists(atPath: destinationUrl.path)
-        if fileExists
-        {
-            //todo:顯示綠色的正在播放
-            
-//            print ("音樂準備中")
-//            print("file Exists:\(destinationUrl)")
-//            // prepare file
-//            prepareAudioFile()
-//
-//            print("file Exists:\(destinationUrl)")
-//            print("播放錄音檔")
-//            play()
-            
-        
-            print(destinationUrl)
-            
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOf: destinationUrl)
-                audioPlayer.volume = 1.0
-                audioPlayer.play()
-            } catch {
-                print("Play error:", error.localizedDescription)
-            }
-            
-//            do{
-//                audioPlayer = try AVAudioPlayer(contentsOf: destinationUrl)
-//                print ("準備播放")
-//                audioPlayer.play()
-//                print ("已播放")
-//            } catch{
-//                print ("error:播放失敗")
-//            }
-            
-        }
-        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
